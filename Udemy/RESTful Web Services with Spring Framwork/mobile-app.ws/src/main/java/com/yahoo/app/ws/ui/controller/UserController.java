92f4c81688a2e2746fd793ecbede5e73dedf833c
lordbyron <byronknox@yahoo.co.uk>
2022-09-06 11:14:52 +0200

    SECTION 7, Reading HTTP POST request body

--- Udemy/RESTful Web Services with Spring Framwork/mobile-app.ws/src/main/java/com/yahoo/app/ws/ui/controller/UserController.java
+++ Udemy/RESTful Web Services with Spring Framwork/mobile-app.ws/src/main/java/com/yahoo/app/ws/ui/controller/UserController.java
@@ -8,25 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yahoo.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.yahoo.app.ws.ui.model.response.UserRest;
 
-//@RestController: This registers this class as a REST controller, and will be able to receive HTTP 
-//requests and match their URL path.
 @RestController
-//@RequestMapping: Controller responsible for alterations that relate to 'users'.
-//http://localhost:8080/users	<- '/users' path added to annotation.
 @RequestMapping("/users")	
 public class UserController {
 	
-//create method that GETs all users.
-//@GetMapping: Binds this method to HTTP GET request.
-//Get Request with parameters eg.: http://localhost:8080/users?page=1&limit=50
-//@RequestParam(value="<something>") int page,... - maps the methods parameter to the parameter passed in the HTTP request.
-//optional - add a default value: @RequestParam(value="page", defaultValue="1")
 	@GetMapping									
 	public String getUsers(@RequestParam(value="page", defaultValue="1") int page, @RequestParam(value="limit", defaultValue="50") int limit)		
 	{														
@@ -34,36 +27,45 @@ public class UserController {
 	}
 	
 
-//create method that GETs users by ID.
 	@GetMapping(
-			path="/{userId}", 							       //path added to binding	so method can read the userId. (/users/userId)
-			produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }  //produces = { MediaType... } = allows return in JSON and XML format.
+			path="/{userId}", 							      
+			produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }  
 			)																
-	public ResponseEntity<UserRest> getUser(@PathVariable String userId)		//PathVariabe that is read from url users/userId will be made available to
-	{															// the methods via an argument. i.e. The user's id
-		UserRest returnValue = new UserRest();					//Create instance of UserRest
-		returnValue.setEmail("test@test.com");					//set values of the object.
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId)		
+	{															
+		UserRest returnValue = new UserRest();					
+		returnValue.setEmail("test@test.com");					
 		returnValue.setFirstName("Sergey");
-		returnValue.setLastName("Kargopolov");					//
-		//ResponseEntity() constructor takes various args depending on the response you want. 
-		// Here we return 'body' (returnValue), and status.
+		returnValue.setLastName("Kargopolov");					
+		
 //		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 		return new ResponseEntity<UserRest>(HttpStatus.BAD_REQUEST);
 		
 	}
 	
 	
-//create method to create users
-//PostMapping: Binds method to HTTP POST request.
-	@PostMapping
-	public String createUser()
+	@PostMapping(
+			consumes = { 									//can consume json & xml.
+			MediaType.APPLICATION_XML_VALUE, 
+			MediaType.APPLICATION_JSON_VALUE },
+			produces = { 									//can produce json & xml.
+					MediaType.APPLICATION_XML_VALUE, 
+					MediaType.APPLICATION_JSON_VALUE }) 
+	//@RequestBody lets this method read info coming in from the HTTP request body.
+	//Accepts object of type UserDetailsRequestModel as an argument, assigns it to 'userDetails'.
+	public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails)
 	{
-		return "create user was called";
+	
+		//create a returnValue object, and set its details.
+		UserRest returnValue = new UserRest();					
+		returnValue.setEmail(userDetails.getEmail());					
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+		
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
 
 	
-//create method to update user details
-//@PutMapping: Binds this method to HTTP PUT request.
 	@PutMapping
 	public String updateUser()
 	{
@@ -71,8 +73,6 @@ public class UserController {
 	}
 	
 	
-//create method to delete users
-//@DeleteMapping: binds this method to HTTP DELETE request.
 	@DeleteMapping
 	public String deleteUser()
 	{
