68960dd142a7fc060e36ac525e59b07998cc6269
lordbyron <byronknox@yahoo.co.uk>
2022-10-04 09:15:58 +0200

    Section 2

---
+++ Udemy/Master RESTful APIs with Spring Boot 2 in 100 Steps/springboot-buildingblocks/src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.stacksimplify.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//controller
+@RestController
+public class HelloWorldController {
+
+	//simple GET method. 
+	//URI/path: /helloworld
+	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")		//this line can be replaced with 
+	@GetMapping("/helloworld")												// @GetMapping("/helloworld")
+	public String helloWorld() {											
+		return "Hello World";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Kalyan", "Reddy", "Hyderabad");
+	}
+	
+}
