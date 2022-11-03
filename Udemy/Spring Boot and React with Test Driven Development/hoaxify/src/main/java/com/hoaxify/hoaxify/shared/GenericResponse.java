6468bc414a47e5ad7e80bed46eec0a1548c8e298
lordbyron <byronknox@yahoo.co.uk>
2022-11-03 10:57:29 +0200

    password hashing

--- Udemy/Spring Boot and React with Test Driven Development/hoaxify/src/main/java/com/hoaxify/hoaxify/shared/GenericResponse.java
+++ Udemy/Spring Boot and React with Test Driven Development/hoaxify/src/main/java/com/hoaxify/hoaxify/shared/GenericResponse.java
@@ -1,10 +1,16 @@
 package com.hoaxify.hoaxify.shared;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data								//lombok annotation that creates getters and setters
+@NoArgsConstructor					//lombok annotation: creates no argument constructor
 public class GenericResponse {
 	
 	 private String message;
+	 
+	 public GenericResponse(String message) {
+		 this.message = message;
+	 }
 
 }
--- Udemy/Spring Boot and React with Test Driven Development/hoaxify/src/main/java/com/hoaxify/hoaxify/user/UserService.java
+++ Udemy/Spring Boot and React with Test Driven Development/hoaxify/src/main/java/com/hoaxify/hoaxify/user/UserService.java
@@ -1,5 +1,6 @@
 package com.hoaxify.hoaxify.user;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service							//tells Spring to create an instance of this class
@@ -7,15 +8,19 @@ public class UserService {
 	
 	UserRepository userRepository;
 	
-//Constructor injection
-//when Spring creates an instance of this class it will call the constructor, so we dont need to autowire userRepository.
+	BCryptPasswordEncoder passwordEncoder;	//initialize the Spring security dependency encoder
+	
+//Constructor injection: when Spring creates an instance of this class it will call the constructor, the constructor is looking 
+//	for 'userRepository	so it provides the userRepository to this service, and we dont need to autowire it in.
 	public UserService(UserRepository userRepository) {
 		super();
 		this.userRepository = userRepository;
+		this.passwordEncoder = new BCryptPasswordEncoder();	
 	}
 	
 	//method that returns a user object. Accepts user object as arg, which it gets from the POST request.
 	public User save(User user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));	//encode the password and set it as the user password.
 		return userRepository.save(user);
 }
 }
