fb2bbeb9e4c12cd7b461d7782c65bfaf6874980a
lordbyron <byronknox@yahoo.co.uk>
2022-11-08 09:21:59 +0200

    User Signup: sending request to backend

--- Udemy/Spring Boot and React with Test Driven Development/frontend/src/index.js
+++ Udemy/Spring Boot and React with Test Driven Development/frontend/src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import { UserSignupPage } from './pages/UserSignupPage';
+import * as apiCalls from './api/apiCalls';
+
+const actions = {             //create object with 'postSignup' field.
+  postSignup: apiCalls.signup //pass 'actions' obj as property to <UserSignupPage>
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserSignupPage />
+    <UserSignupPage actions={actions} />  
   </React.StrictMode>
 );
 
